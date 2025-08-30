b7453d8b41d33ee4a9fb77666b37379fef3d015f
Coday-aw <138451310+Coday-aw@users.noreply.github.com>
2025-08-30 10:32:30 +0200

    Add mobile app

---
+++ mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,29 @@
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function TabLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="home" size={28} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Create",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="add" size={28} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+    </Tabs>
+  );
+}
